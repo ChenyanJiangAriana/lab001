e6994fc7066441d266327bd9cfd7463978de6851
Jiang Chenyan <624071310@qq.com>
2020-09-29 19:01:08 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -5,7 +5,34 @@ const aCar = {
         model : "Corolla",
         CC: 1.8
     },
-    registration : '201WD1058'
+    registration : {year: '201',
+    county: 'WD',
+    number: '1058'}
 };
 
+// Add a new property MILEAGE
+aCar.mileage = 1000;
+// Add a new property COLOR
+aCar.color ={
+    exterior: "red",
+    interior:{texture: "leather", shade: "cream"}
+
+}
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log(
+    "Reg. = "+
+       aCar.registration.year +
+       " " +
+       aCar.registration.county +
+       " " + 
+       aCar.registration.number
+)
+console.log("It is a " +
+        aCar.color.exterior +
+        "," +
+        aCar.mileage +
+        " " + "mileage"+
+        "," + "with "+
+        aCar.color.interior.texture+
+        " interior"
+)
