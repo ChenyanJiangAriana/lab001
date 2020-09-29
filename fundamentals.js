97f30af7529db8955ed7e4e2d0c94c4bd134c19a
Jiang Chenyan <624071310@qq.com>
2020-09-29 19:12:25 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,10 +1,15 @@
 const aCar = {
     owner : "Joe Bloggs",
+
+    previous_owners:[{ name:"Pat Smith ", address: "1 Main Street"},
+        {name: "Sheila Dwyer ", address: "2 High Street"}] ,
+
     type : {
         make : 'Toyota',
         model : "Corolla",
         CC: 1.8
     },
+
     registration : {year: '201',
     county: 'WD',
     number: '1058'}
@@ -34,5 +39,6 @@ console.log("It is a " +
         " " + "mileage"+
         "," + "with "+
         aCar.color.interior.texture+
-        " interior"
+        " interior."
 )
+console.log('First owner : ' + aCar.previous_owners[0].name+ "-" +aCar.previous_owners[0].address)
